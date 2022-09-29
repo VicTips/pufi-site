1823c3b399dd45915c3acf06b4786c60f3d5d579
victips <torvicarias@gmail.com>
2022-09-29 09:13:17 -0500

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import Header from "./components/header/Header"
 import Cards from "./components/cards/Cards"
 import Social from "./components/social/Social"
 import Newsletter from "./components/newsletter/Newsletter"
 import Footer from "./components/footer/Footer"
 
 const App = () => {
-  return <div>Hola
-    <Cards />
-    <Social />
-    <Newsletter />
-    <Footer />
-  </div>;
+  return (
+    <div>
+      <Header />
+      <Cards />
+      <Social />
+      <Newsletter />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,64 @@
 import React from 'react'
+import styles from "./Navbar.module.css"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className={styles.container}>
+      <div className={styles.navbar}>
+        <div className={styles.logo}>
+          Pufi<sup>®</sup>
+        </div>
+        <div className={styles.categories}>
+          <div>
+            <div className={styles.category_icon}>
+              <ion-icon name="bed-outline"></ion-icon>
+            </div>
+            <div className={styles.category_name}>
+              PUFI PUFF
+            </div>
+          </div>
+          <div className={styles.divider}></div>
+          <div>
+            <div className={styles.category_icon}>
+              <ion-icon name="umbrella-outline"></ion-icon>
+            </div>
+            <div className={styles.category_name}>
+              PUFI RAIN
+            </div>
+          </div>
+          <div className={styles.divider}></div>
+          <div>
+            <div className={styles.category_icon}>
+              <ion-icon name="bag-outline"></ion-icon>
+            </div>
+            <div className={styles.category_name}>
+              PUFI CART
+            </div>
+          </div>
+          <div className={styles.divider}></div>
+          <div>
+            <div className={styles.category_icon}>
+              <ion-icon name="easel-outline"></ion-icon>
+            </div>
+            <div className={styles.category_name}>
+              PUFI NAP
+            </div>
+          </div>
+        </div>
+        <div className={styles.links}>
+          <div className={styles.account}>
+            MI CUENTA
+            <div className={styles.account_icon}>
+              <ion-icon name="chevron-down-outline"></ion-icon>
+            </div>
+          </div>
+          <div className={styles.divider_2}></div>
+          <div>
+            MI COMPRA
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
