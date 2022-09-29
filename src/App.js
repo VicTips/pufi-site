91f088db6aaff6a1ccdd28b7aa410b1b43e573ea
victips <torvicarias@gmail.com>
2022-09-28 20:55:51 -0500

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Newsletter from "./components/newsletter/Newsletter"
 import Footer from "./components/footer/Footer"
 
 const App = () => {
   return <div>Hola
+    <Newsletter />
     <Footer />
   </div>;
 };
--- src/components/newsletter/Newsletter.js
+++ src/components/newsletter/Newsletter.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import styles from "./Newsletter.module.css"
 
 const Newsletter = () => {
   return (
-    <div>Newsletter</div>
+    <section className={styles.container}>
+      <div>
+        <div className={styles.section_title}>
+          NEWSLETTER
+        </div>
+        <h1>
+          SUSCRIBITE
+        </h1>
+        <p>
+          Y enterate de todas las novedades
+        </p>
+        <div className={styles.form}>
+          <input type="email" placeholder='Ingresa tu email' />
+          <button className={styles.arrow}>
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </button>
+        </div>
+      </div>
+    </section>
   )
 }
 
